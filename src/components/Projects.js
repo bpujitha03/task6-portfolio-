9125fc72ba7d32f7b34a910d11453c8100bf2df8
pujitha <bpujitha7@gmail.com>
2023-10-14 17:27:30 +0530

    first commit

---
+++ src/components/Projects.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Projects = () => {
+  return (
+    <section id="projects">
+      <h2>Projects</h2>
+      {<p> - Silver Ecommers -  Basic Disney Theme Website 
+
+      </p>
+    
+      }
+    </section>
+  );
+}
+
+export default Projects;
---
+++ src/components/contact.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <section>
+      <h2>Contact</h2>
+      <p>
+        I'm always open to new opportunities and collaborations. 
+      </p>
+      <ul>
+        <li>
+          <strong>Email:</strong> <a>bpujitha7@gmail.com</a>
+        </li>
+        <li>
+          <strong>LinkedIn:</strong> <a href="https://github.com/bpujitha03 " target="_blank" rel="noopener noreferrer">Linkedin</a>
+        </li>
+        <li>
+          <strong>GitHub:</strong> <a href="https://github.com/bpujitha03 " target="_blank" rel="noopener noreferrer">github</a>
+        </li>
+       
+      </ul>
+      <p>
+        I'm looking forward to connecting with you!
+      </p>
+    </section>
+  );
+};
+
+export default Contact;
